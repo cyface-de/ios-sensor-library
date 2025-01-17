b46e8a0cb40fa11e75e761d073035bda9aeabf55
Klemens Muthmann <klemens.muthmann@cyface.de>
2025-01-17 13:48:28 +0100

    Add function to get ordered upload tasks from UploadSession

---
+++ Sources/DataCapturing/Model/UploadSession+ManualExtensions.swift
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025 Cyface GmbH
+ *
+ * This file is part of the Cyface SDK for iOS.
+ *
+ * The Cyface SDK for iOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Cyface SDK for iOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the Cyface SDK for iOS. If not, see <http://www.gnu.org/licenses/>.
+ */
+import Foundation
+
+extension UploadSession {
+    public func typedOrderedUploadProtocol() -> [UploadTask] {
+        if let converted = uploadProtocol?.array as? [UploadTask] {
+            return converted.sorted { $0.time ?? Date.distantPast < $1.time ?? Date.distantPast }
+        } else {
+            return [UploadTask]()
+        }
+    }
+}
