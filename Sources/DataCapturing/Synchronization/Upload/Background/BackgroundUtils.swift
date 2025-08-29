8622d4430e2a114fed8cd86736e29c47e5064aa7
Klemens Muthmann <klemens.muthmann@cyface.de>
2025-08-29 14:00:08 +0200

    Add method to store files into sandbox storage.

--- Sources/DataCapturing/Synchronization/Upload/Background/BackgroundUtils.swift
+++ Sources/DataCapturing/Synchronization/Upload/Background/BackgroundUtils.swift
@@ -47,3 +47,31 @@ func copyToTemp(data: Data, filename: String) throws -> URL {
     try data.write(to: target)
     return target
 }
+
+/**
+ Speichert gegebene Daten in einer Datei im Documents-Verzeichnis der App.
+
+ - Parameters:
+   - data: Das `Data`-Objekt, das gespeichert werden soll.
+   - filename: Der Name für die zu erstellende Datei (z.B. "upload-123.json").
+ - Throws: Wirft einen Fehler, wenn das Documents-Verzeichnis nicht gefunden oder die Datei nicht geschrieben werden kann.
+ - Returns: Die `URL` zur neu erstellten Datei.
+*/
+func saveToDocuments(data: Data, with filename: String) throws -> URL {
+    // 1. Find the URL to the users documents directory
+    guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+        // This error should not occur.
+        throw NSError(domain: "de.cyface.fileerror", code: 1, userInfo: [NSLocalizedDescriptionKey: "Documents-Directory not found."])
+    }
+
+    // 2. Create complete URL for the file
+    let fileURL = documentsDirectory.appendingPathComponent(filename)
+
+    // 3. Write data to that location
+    // Atomic ensures the file is only written if that can be done completely and without error.
+    try data.write(to: fileURL, options: .atomic)
+
+    print("File save to: \(fileURL.path)")
+
+    return fileURL
+}
